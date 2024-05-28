77cd0f6abd2d0309d4737919f32a5119fcb83894
RakeshDevCode <146819010+RakeshDevCode@users.noreply.github.com>
2024-05-28 22:27:28 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)  we are using node js not browser
+
+console.log(2+2)
+
+console.log("Mumbai")
+
+const number =123
+const name = "Name"
+console.log(typeof undefined) //undefined 
+console.log(typeof nudll) // object
